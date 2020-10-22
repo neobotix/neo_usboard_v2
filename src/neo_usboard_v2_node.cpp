24edd03afea3678e51fce69cb228ac7eb9c70f22
pradheep <padhupradheep@gmail.com>
2020-10-22 13:51:34 +0200

    USSboard messages are published in ROS Topics

--- src/neo_usboard_v2_node.cpp
+++ src/neo_usboard_v2_node.cpp
@@ -8,18 +8,21 @@
 #include <ros/ros.h>
 
 #include <vnx/vnx.h>
-
 #include <pilot/base/CAN_Proxy.h>
 #include <pilot/base/SerialPort.h>
 #include <pilot/usboard/USBoardModule.h>
 #include <pilot/usboard/USBoardModuleClient.hxx>
-
+#include <neo_msgs/USBoardV2.h>
+#include <sensor_msgs/Range.h>
 #include <neo_usboard_v2/package.hxx>
 #include <neo_usboard_v2/ROS_NodeBase.hxx>
 
 
 class ROS_Node : public neo_usboard_v2::ROS_NodeBase {
 public:
+	ros::NodeHandle nh;
+	ros::Publisher topicPub_usBoard = nh.advertise<neo_msgs::USBoardV2>("/usboard_v2/measurements",1);
+	ros::Publisher topicPub_USRangeSensor[16];
 	ROS_Node(const std::string& _vnx_name)
 		:	ROS_NodeBase(_vnx_name),
 			usboard_sync("USBoardModule")
@@ -43,6 +46,35 @@ protected:
 	void handle(std::shared_ptr<const pilot::usboard::USBoardData> value) override
 	{
 		// TODO: convert and publish on ROS
+		neo_msgs::USBoardV2 usBoard;
+		usBoard.header.stamp = ros::Time::now();
+		usBoard.header.frame_id = "USSensors";
+		
+		for(int i=0; i < 16; i++)
+		{
+			sensor_msgs::Range USRangeMsg;
+			usBoard.active[i] = config->sensor_config[i].active;
+			USRangeMsg.header.stamp = ros::Time::now(); 		//time
+			USRangeMsg.header.frame_id = "usrangesensor"+std::to_string(i);		//string
+			USRangeMsg.radiation_type = 0; 			//uint8   => Enum ULTRASOUND=0; INFRARED=1
+			USRangeMsg.field_of_view = 2.3; 		//float32 [rad]
+			USRangeMsg.min_range = 0.2; 			//float32 [m]
+			USRangeMsg.max_range = 3.0; 			//float32 [m]
+			USRangeMsg.range = value->sensor[i]; 	//float32 [m]
+
+			//publish data for first USrange sensor
+			if(config->sensor_config[i].active)
+			{
+				topicPub_USRangeSensor[i].publish(USRangeMsg);
+			}
+			usBoard.sensor[i] = value->sensor[i];
+		}
+
+		for(int i = 0; i < 4; i++)
+		{
+			usBoard.analog[i] = value->analog_input[i];
+		}
+		topicPub_usBoard.publish(usBoard);
 	}
 
 	void handle(std::shared_ptr<const pilot::usboard::USBoardConfig> value) override
@@ -58,6 +90,7 @@ protected:
 			if(sensor.active) {
 				sensor_group_enable[i / 4] = true;		// auto enable group for requests
 			}
+			topicPub_USRangeSensor[i] = nh.advertise<sensor_msgs::Range>("/usboard_v2/sensor"+std::to_string(i),1);
 			i++;
 		}
 		config = value;
@@ -92,7 +125,6 @@ int main(int argc, char** argv)
 	// initialize VNX
 	vnx::init("neo_usboard_v2_node", 0, nullptr);
 
-	ros::NodeHandle nh;
 	ros::NodeHandle nh_private("~");
 
 	std::string can_device;
@@ -152,5 +184,3 @@ int main(int argc, char** argv)
 
 	return 0;
 }
-
-
