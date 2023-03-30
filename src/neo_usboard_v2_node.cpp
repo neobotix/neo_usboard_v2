614c66cc40ca9a9b0421d53134506988923205ab
Pradheep-office <padhupradheep@gmail.com>
2023-03-30 18:45:15 +0200

    update for sending complete param

--- src/neo_usboard_v2_node.cpp
+++ src/neo_usboard_v2_node.cpp
@@ -16,29 +16,119 @@
 #include <sensor_msgs/Range.h>
 #include <neo_usboard_v2/package.hxx>
 #include <neo_usboard_v2/ROS_NodeBase.hxx>
+#include <dynamic_reconfigure/server.h>
+#include "neo_usboard_v2/neo_usboard_v2Config.h"
 
 
-class ROS_Node : public neo_usboard_v2::ROS_NodeBase {
+class ROS_Node : public neo_usboard_v2::ROS_NodeBase, public ros::NodeHandle {
 public:
-	ros::NodeHandle nh;
+	ros::NodeHandle nh_;
 	ros::Publisher topicPub_usBoard;
 	ros::Publisher topicPub_USRangeSensor[16];
 
-	ROS_Node(const std::string& _vnx_name)
+	ROS_Node(const std::string& _vnx_name, ros::NodeHandle *nh)
 		:	ROS_NodeBase(_vnx_name),
 			usboard_sync("USBoardModule")
 	{
+		
+		nh->param<std::string>("can_device", can_device, "");
+		nh->param<std::string>("serial_port", serial_port, "/dev/ttyUSB0");
+		nh->param<std::string>("topic_path", topic_path, "/usboard_v2");
+		nh->param<int>("can_id", can_id, 1024);
+		nh->param<int>("can_baud_rate", can_baud_rate, 1000000);
+		nh->param<double>("update_rate", update_rate, 5.);
+		nh->param<std::vector<bool>>("active_sensors", active_sensors, active_sensors);
+		nh->param<std::vector<double>>("warn_distance", warn_distance, warn_distance);
+		nh->param<std::vector<double>>("alarm_distance", alarm_distance, alarm_distance);
+
+		nh->param<std::vector<bool>>("enable_transmission", enable_transmission, enable_transmission);
+		nh->param<std::vector<int>>("resolution", resolution, resolution);
+		nh->param<std::vector<int>>("fire_interval_ms", fire_interval_ms, fire_interval_ms);
+		nh->param<std::vector<int>>("sending_sensor", sending_sensor, sending_sensor);
+		nh->param<std::vector<bool>>("cross_echo_mode", cross_echo_mode, cross_echo_mode);
+
+		nh->param<double>("low_pass_gain", low_pass_gain, 0.0);
+		nh->param<bool>("enable_analog_input", enable_analog_input, false);
+		nh->param<bool>("enable_legacy_format", enable_legacy_format, false);
+		nh->param<bool>("enable_can_termination", enable_can_termination, false);
+		nh->param<bool>("relay_warn_blocked_invert", relay_warn_blocked_invert, false);
+		nh->param<bool>("relay_alarm_blocked_invert", relay_alarm_blocked_invert, false);
+		nh_ = *nh;
+	}
+
+	void set_ros_params(std::shared_ptr<const pilot::usboard::USBoardConfig> value)
+	{
+		// prevent our own changes from triggering lots of param updates
+		ros_params_initialized = false;
+
+		serial_number = value->serial_number;
+		hardware_version = value->hardware_version;
+		can_id = value->can_id;
+		can_baud_rate = value->can_baudrate;
+		update_rate = value->update_interval_ms;
+		
+		for (auto i = 0; i < active_sensors.size(); i++) {
+			active_sensors[i] = value->sensor_config[i].active;
+			warn_distance[i] = value->sensor_config[i].warn_distance;
+			alarm_distance[i] = value->sensor_config[i].alarm_distance;
+		}
+
+		for (auto i = 0; i < enable_transmission.size(); i++) {
+			enable_transmission[i] = value->group_config[i].enable_transmission;
+			resolution[i] = value->group_config[i].resolution;
+			fire_interval_ms[i] = value->group_config[i].fire_interval_ms;
+			sending_sensor[i] = value->group_config[i].sending_sensor;
+			cross_echo_mode[i] = value->group_config[i].cross_echo_mode;
+		}
+		
+		low_pass_gain = value->low_pass_gain;
+		enable_analog_input = value->enable_analog_input;
+		enable_legacy_format = value->enable_legacy_format;
+		enable_can_termination = value->enable_can_termination;
+		relay_warn_blocked_invert = value->relay_warn_blocked_invert;
+		relay_alarm_blocked_invert = value->relay_alarm_blocked_invert;
+
+		nh_.setParam("hardware_version", hardware_version);
+		nh_.setParam("serial_number", serial_number);
+
+		nh_.setParam("can_id", can_id);
+		nh_.setParam("can_baud_rate", can_baud_rate);
+		nh_.setParam("update_rate", update_rate);
+
+		nh_.setParam("active_sensors", active_sensors);
+		nh_.setParam("warn_distance", warn_distance);
+		nh_.setParam("alarm_distance", alarm_distance);
+
+		nh_.setParam("enable_transmission", enable_transmission);
+		nh_.setParam("resolution", resolution);
+		nh_.setParam("fire_interval_ms", fire_interval_ms);
+		nh_.setParam("sending_sensor", sending_sensor);
+		nh_.setParam("cross_echo_mode", cross_echo_mode);
+
+		nh_.setParam("low_pass_gain", low_pass_gain);
+		nh_.setParam("enable_analog_input", enable_analog_input);
+		nh_.setParam("enable_legacy_format", enable_legacy_format);
+		nh_.setParam("enable_can_termination", enable_can_termination);
+		nh_.setParam("relay_warn_blocked_invert", relay_warn_blocked_invert);
+		nh_.setParam("relay_alarm_blocked_invert", relay_alarm_blocked_invert);
+
+		ros_params_initialized = true;
 	}
 
 protected:
 	void main() override
 	{
-		subscribe(input_data);
-		subscribe(input_config);
+		neo_usboard_v2::ROS_NodeBase::subscribe(input_data);
+		neo_usboard_v2::ROS_NodeBase::subscribe(input_config);
 
 		set_timer_millis(1000, std::bind(&ROS_Node::request_config, this));
 
-		topicPub_usBoard = nh.advertise<neo_msgs::USBoardV2>(topic_path + "/measurements", 1);
+		topicPub_usBoard = nh_.advertise<neo_msgs::USBoardV2>(topic_path + "/measurements", 1);
+
+    	server = new dynamic_reconfigure::Server<neo_usboard_v2::neo_usboard_v2Config>(nh_);	
+		dynamic_reconfigure::Server<neo_usboard_v2::neo_usboard_v2Config>::CallbackType cb=
+			[this](neo_usboard_v2::neo_usboard_v2Config& config, uint32_t level){ reconfigureCB(config, level); };
+    	server->setCallback(cb);
 
 		Super::main();
 
@@ -93,7 +183,7 @@ protected:
 		{
 			if(sensor.active) {
 				sensor_group_enable[i / 4] = true;		// auto enable group for requests
-				topicPub_USRangeSensor[i] = nh.advertise<sensor_msgs::Range>(topic_path + "/sensor" + std::to_string(i), 1);
+				topicPub_USRangeSensor[i] = nh_.advertise<sensor_msgs::Range>(topic_path + "/sensor" + std::to_string(i), 1);
 			}
 			i++;
 		}
@@ -104,6 +194,8 @@ protected:
 
 	void update()
 	{
+		std::lock_guard<std::mutex> lock(mutex_usboard_sync);
+
 		// request sensor data
 		usboard_sync.request_data(std::vector<bool>(sensor_group_enable.begin(), sensor_group_enable.end()));
 
@@ -124,10 +216,123 @@ protected:
 		}
 	}
 
+	void set_pilot_params(std::shared_ptr<pilot::usboard::USBoardConfig> value) const
+	{
+		value->can_id = can_id;
+		value->can_baudrate = can_baud_rate;
+		value->update_interval_ms = update_rate;
+		
+		for (auto i = 0; i < active_sensors.size(); i++) {
+			value->sensor_config[i].active = active_sensors[i];
+			value->sensor_config[i].warn_distance = warn_distance[i];
+			value->sensor_config[i].alarm_distance = alarm_distance[i];
+		}
+
+		for (auto i = 0; i < enable_transmission.size(); i++) {
+			value->group_config[i].enable_transmission = enable_transmission[i];
+			value->group_config[i].resolution = resolution[i];
+			value->group_config[i].fire_interval_ms = fire_interval_ms[i];
+			value->group_config[i].sending_sensor = sending_sensor[i];
+			value->group_config[i].cross_echo_mode = cross_echo_mode[i];
+		}
+		
+		value->low_pass_gain = low_pass_gain;
+		value->enable_analog_input = enable_analog_input;
+		value->enable_legacy_format = enable_legacy_format;
+		value->enable_can_termination = enable_can_termination;
+		value->relay_warn_blocked_invert = relay_warn_blocked_invert;
+		value->relay_alarm_blocked_invert = relay_alarm_blocked_invert;
+	}
+
+	void reconfigureCB(neo_usboard_v2::neo_usboard_v2Config &val, uint32_t level)
+	{
+		active_sensors = {val.a_s0, val.a_s1, val.a_s2, val.a_s3, val.a_s4,
+			val.a_s5, val.a_s6, val.a_s7, val.a_s8, val.a_s9, val.a_s10,
+			val.a_s11, val.a_s12, val.a_s13, val.a_s14, val.a_s15 };
+		
+		warn_distance = {val.w_d0, val.w_d1, val.w_d2, val.w_d3, val.w_d4,
+			val.w_d5, val.w_d6, val.w_d7, val.w_d8, val.w_d9, val.w_d10,
+			val.w_d11, val.w_d12, val.w_d13, val.w_d14, val.w_d15 };
+		
+		alarm_distance = {val.a_d0, val.a_d1, val.a_d2, val.a_d3, val.a_d4,
+			val.a_d5, val.a_d6, val.a_d7, val.a_d8, val.a_d9, val.a_d10,
+			val.a_d11, val.a_d12, val.a_d13, val.a_d14, val.a_d15 };
+
+		enable_transmission = {val.ent_0, val.ent_1, val.ent_2, val.ent_3};
+		fire_interval_ms = {val.ft_0, val.ft_1, val.ft_2, val.ft_3};
+		sending_sensor = {val.sn_0, val.sn_1, val.sn_2, val.sn_3};
+		cross_echo_mode = {val.ee_0, val.ee_1, val.ee_2, val.ee_3};
+
+		can_device = val.can_device;
+		serial_port = val.serial_port;
+		topic_path = val.topic_path;
+		serial_number = val.serial_number;
+		hardware_version = val.hardware_version;
+		can_id = val.can_id;
+		can_baud_rate = val.can_baud_rate;
+		update_rate = val.update_rate;
+
+		low_pass_gain = val.low_pass_gain;
+		enable_analog_input = val.enable_analog_input;
+		enable_legacy_format = val.enable_legacy_format;
+		enable_can_termination = val.enable_can_termination;
+		relay_warn_blocked_invert = val.relay_warn_blocked_invert;
+		relay_alarm_blocked_invert = val.relay_alarm_blocked_invert;
+
+		std::shared_ptr<pilot::usboard::USBoardConfig> new_config = vnx::clone(config);
+		set_pilot_params(new_config);
+		{
+			std::lock_guard<std::mutex> lock(mutex_usboard_sync);
+			std::cout << "Updating parameter config: " << new_config->to_string() << std::endl;
+			try{
+				usboard_sync.send_config(new_config);
+				config = new_config;
+			}catch(const std::exception &err){
+				ROS_WARN("Sending USBoard config failed with: %s", err.what());
+			}
+		}
+	}
+
 private:
 	pilot::usboard::USBoardModuleClient usboard_sync;
 
 	std::shared_ptr<const pilot::usboard::USBoardConfig> config;
+	dynamic_reconfigure::Server<neo_usboard_v2::neo_usboard_v2Config> *server;
+	std::mutex mutex_usboard_sync;
+
+public:
+	std::string can_device = "None";
+	std::string serial_port = "/dev/ttyUSB0";
+	std::string topic_path = "/usboard_v2";
+
+	int serial_number;
+	int hardware_version;
+
+	int can_id = 0;
+	int can_baud_rate = 0;
+	double update_rate = 0.0;
+	bool ros_params_initialized = false;
+	
+	// Our new generation supports 16 sensors. Therefore the size of the vector won't go beyond 16
+
+	std::vector<bool> active_sensors = std::vector<bool>(16, false);
+	std::vector<double> warn_distance = std::vector<double>(16, 0.0);
+	std::vector<double> alarm_distance = std::vector<double>(16, 0.0);
+
+	// There are 4 sensor groups, following params deals with that
+
+	std::vector<bool> enable_transmission = std::vector<bool>(4, false);
+	std::vector<int> resolution = std::vector<int>(4, 0);
+	std::vector<int> fire_interval_ms = std::vector<int>(4, 0);
+	std::vector<int> sending_sensor = std::vector<int>(4, 0);
+	std::vector<bool> cross_echo_mode = std::vector<bool>(4, false);
+
+	double low_pass_gain = 0.0;
+	bool enable_analog_input = false;
+	bool enable_legacy_format = false;
+	bool enable_can_termination = false;
+	bool relay_warn_blocked_invert = false;
+	bool relay_alarm_blocked_invert = false;
 
 };
 
@@ -142,43 +347,31 @@ int main(int argc, char** argv)
 
 	ros::NodeHandle nh;
 
-	std::string can_device;
-	std::string serial_port;
-	std::string topic_path;
-	int can_id = 0;
-	int can_baud_rate = 0;
-	double update_rate = 0;
+	auto ros_node = new ROS_Node("neo_usboard_v2_node", &nh);
 
-	nh.param<std::string>("can_device", can_device, "");
-	nh.param<std::string>("serial_port", serial_port, "/dev/ttyUSB0");
-	nh.param<std::string>("topic_path", topic_path, "/usboard_v2");
-	nh.param<int>("can_id", can_id, 1024);
-	nh.param<int>("can_baud_rate", can_baud_rate, 1000000);
-	nh.param<double>("update_rate", update_rate, 5.);
-
-	if(!can_device.empty())
+	if(ros_node->can_device != "None")
 	{
 		vnx::Handle<pilot::base::CAN_Proxy> module = new pilot::base::CAN_Proxy("CAN_Proxy");
-		module->device = can_device;
-		module->baud_rate = can_baud_rate;
+		module->device = ros_node->can_device;
+		module->baud_rate = ros_node->can_baud_rate;
 		module->input = neo_usboard_v2::can_request;
 		module->output = neo_usboard_v2::can_frames;
 		module.start_detached();
-		ROS_INFO_STREAM("Started " << module.get_name() << " on " << can_device);
+		std::cout<<"Started " << module.get_name() << " on " << ros_node->can_device <<std::endl;
 	}
-	else if(!serial_port.empty())
+	else if(!ros_node->serial_port.empty())
 	{
 		vnx::Handle<pilot::base::SerialPort> module = new pilot::base::SerialPort("SerialPort");
-		module->port = serial_port;
+		module->port = ros_node->serial_port;
 		module->baud_rate = 19200;
 		module->raw_mode = true;
 		module->input = neo_usboard_v2::serial_request;
 		module->output = neo_usboard_v2::serial_data;
 		module.start_detached();
-		ROS_INFO_STREAM("Started " << module.get_name() << " on " << serial_port);
+		std::cout<<"Started " << module.get_name() << " on " << ros_node->serial_port <<std::endl;
 	}
 	else {
-		ROS_WARN("No serial port or CAN device configured!");
+		std::cout<<"No serial port or CAN device configured!"<<std::endl;
 	}
 
 	{
@@ -189,15 +382,15 @@ int main(int argc, char** argv)
 		module->topic_serial_request = neo_usboard_v2::serial_request;
 		module->output_data = neo_usboard_v2::data;
 		module->output_config = neo_usboard_v2::config;
-		module->can_id = can_id;
+		module->can_id = ros_node->can_id;
 		module.start_detached();
 	}
 	{
-		vnx::Handle<ROS_Node> module = new ROS_Node("ROS_Node");
+		vnx::Handle<ROS_Node> module = ros_node;
 		module->input_data = neo_usboard_v2::data;
 		module->input_config = neo_usboard_v2::config;
-		module->update_interval_ms = 1000 / update_rate;
-		module->topic_path = topic_path;
+		module->update_interval_ms = 1000 / ros_node->update_rate;
+		module->topic_path = ros_node->topic_path;
 		module.start_detached();
 	}
 
